6ea428b1d9a2b37b242a541419ae3588ca544911
Florent Solt <florent@solt.biz>
2015-09-17 11:46:40 +0200

    Remove credits (now in the image)

--- public/javascripts/9_run.js
+++ public/javascripts/9_run.js
@@ -1,11 +1,4 @@
 window.addEvent('domready', function() {
   Application.init();
   Command.init();
-  if (Cookie.read('bg_id') && Cookie.read('bg_user')) {
-    var credits = $('credits');
-    var a = new Element('a', {
-      'href': 'http://500px.com/photo/' + Cookie.read('bg_id'),
-      'text': '© ' + Cookie.read('bg_user').replace(/\+/g," ")
-    }).inject(credits)
-  }
 });
