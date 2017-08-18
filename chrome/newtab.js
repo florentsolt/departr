37b3903f37d4ca92db28f16bff06f6ca22704857
Florent Solt <florent@solt.biz>
2017-08-18 16:14:44 +0200

    improved version with clock and css animation

---
+++ chrome/newtab.js
@@ -0,0 +1,46 @@
+var hands = [];
+hands.push(document.querySelector('#secondhand > *'));
+hands.push(document.querySelector('#minutehand > *'));
+hands.push(document.querySelector('#hourhand > *'));
+
+var cx = 100;
+var cy = 100;
+
+function shifter(val) {
+  return [val, cx, cy].join(' ');
+}
+
+var date = new Date();
+var hoursAngle = 360 * date.getHours() / 12 + date.getMinutes() / 2;
+var minuteAngle = 360 * date.getMinutes() / 60;
+var secAngle = 360 * date.getSeconds() / 60;
+
+hands[0].setAttribute('from', shifter(secAngle));
+hands[0].setAttribute('to', shifter(secAngle + 360));
+hands[1].setAttribute('from', shifter(minuteAngle));
+hands[1].setAttribute('to', shifter(minuteAngle + 360));
+hands[2].setAttribute('from', shifter(hoursAngle));
+hands[2].setAttribute('to', shifter(hoursAngle + 360));
+
+for(var i = 1; i <= 12; i++) {
+  var el = document.createElementNS('http://www.w3.org/2000/svg', 'line');
+  el.setAttribute('x1', '100');
+  el.setAttribute('y1', '30');
+  el.setAttribute('x2', '100');
+  el.setAttribute('y2', '40');
+  el.setAttribute('transform', 'rotate(' + (i*360/12) + ' 100 100)');
+  el.setAttribute('style', 'stroke: #ffffff;');
+  document.querySelector('svg').appendChild(el);
+}
+
+var options = {  
+  weekday: "long", year: "numeric", month: "long", day: "numeric"
+}; 
+
+var cb = () => {
+  var date = new Date();
+  document.querySelector('#text').innerHTML = date.toLocaleDateString(false, options) + "<br>" + date.toLocaleTimeString(options);
+}
+
+setInterval(cb, 1000);
+cb();
